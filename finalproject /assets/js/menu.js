8c6ab0397e956ce13485c02103ca1eb72b79d696
ryleewarmington <62074984+ryleewarmington@users.noreply.github.com>
2020-05-01 16:18:50 -0700

    final project p2

---
+++ finalproject /assets/js/menu.js
@@ -0,0 +1,9 @@
+let raincloudelement = document.getElementsByClassName('raincloud');
+function clouds (){
+   if (x.style.display === "none") {
+     x.style.display = "block";
+   } else {
+     x.style.display = "none";
+   }
+ }
+raincloudelement.onclick = coulds;
